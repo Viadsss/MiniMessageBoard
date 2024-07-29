8e548d69a5fdcad3f29519e8512af083b61fc5d8
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-07-30 00:13:03 +0800

    Add stylings & fix bugs

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -4,6 +4,9 @@ const messages = require("../db/messages");
 
 router.get("/:id", (req, res) => {
   const message = messages[req.params.id];
+  if (!message) {
+    return res.status(404).send("Message not found");
+  }
   res.render("messageDetail", { title: "Message Details", message: message });
 });
 
